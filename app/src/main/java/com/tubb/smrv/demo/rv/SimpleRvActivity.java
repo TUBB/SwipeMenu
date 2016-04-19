aaead99b821d8c4d43dc4a95e6a72c1ada860a6c
George Kankava <george.kankava@devfactory.com>
2016-04-19 13:59:01 +0400

    Multiple code improvements - common-java:DuplicatedBlocks, squid:S00122, squid:S1126

--- app/src/main/java/com/tubb/smrv/demo/rv/SimpleRvActivity.java
+++ app/src/main/java/com/tubb/smrv/demo/rv/SimpleRvActivity.java
@@ -2,103 +2,49 @@ package com.tubb.smrv.demo.rv;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tubb.smrv.SwipeMenuLayout;
-import com.tubb.smrv.SwipeMenuRecyclerView;
-import com.tubb.smrv.demo.BaseActivity;
+import com.tubb.smrv.demo.AbstractRvActivity;
 import com.tubb.smrv.demo.R;
 import com.tubb.smrv.demo.User;
 
 import java.util.List;
 
-public class SimpleRvActivity extends BaseActivity {
-
-    private Context mContext;
-    private List<User> users;
-    private AppAdapter mAdapter;
-    private SwipeMenuRecyclerView mRecyclerView;
-    private SwipeRefreshLayout swipeRefreshLayout;
+public class SimpleRvActivity extends AbstractRvActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
-        mContext = this;
-        users = getUsers();
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                Toast.makeText(mContext, "Refresh success", Toast.LENGTH_LONG).show();
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        });
-        mRecyclerView = (SwipeMenuRecyclerView) findViewById(R.id.listView);
-        mRecyclerView.addItemDecoration(new VerticalSpaceItemDecoration(3));
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new AppAdapter(this, users);
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new VerticalSpaceItemDecoration(3));
+    }
+
+    @Override
+    protected AbstractRvAdapter createAppAdapter(AbstractRvActivity baseRvActivity, List<User> users) {
+        return new SimpleRvAppAdapter(baseRvActivity, users);
     }
 
 
-    class AppAdapter extends RecyclerView.Adapter {
+    private class SimpleRvAppAdapter extends AbstractRvAdapter {
 
-        private static final int VIEW_TYPE_ENABLE = 0;
-        private static final int VIEW_TYPE_DISABLE = 1;
-
-        List<User> users;
-
-        public AppAdapter(Context context, List<User> users){
-            this.users = users;
+        public SimpleRvAppAdapter(Context context, List<User> users) {
+            super(context, users);
         }
 
         @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public int getItemCount() {
-            return users.size();
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            User user = users.get(position);
-            if(user.userId % 2 == 0){
-                return VIEW_TYPE_DISABLE;
-            }else{
-                return VIEW_TYPE_ENABLE;
-            }
-        }
-
-        public boolean swipeEnableByViewType(int viewType) {
-            if(viewType == VIEW_TYPE_ENABLE) return true;
-            else if(viewType == VIEW_TYPE_DISABLE) return false;
-            else return true; // default
-        }
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(mContext).inflate(R.layout.item_simple, parent, false);
-            return new MyViewHolder(itemView);
+        protected RecyclerView.ViewHolder createViewHolder(View itemView) {
+            return new SimpleRvViewHolder(itemView);
         }
 
         @Override
         public void onBindViewHolder(final RecyclerView.ViewHolder vh, final int position) {
             final User user = users.get(position);
-            final MyViewHolder myViewHolder = (MyViewHolder)vh;
+            final SimpleRvViewHolder myViewHolder = (SimpleRvViewHolder)vh;
             myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -143,7 +89,7 @@ public class SimpleRvActivity extends BaseActivity {
 
 
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+    public static class SimpleRvViewHolder extends RecyclerView.ViewHolder{
         TextView tvName;
         TextView tvSwipeEnable;
         View btGood;
@@ -151,7 +97,7 @@ public class SimpleRvActivity extends BaseActivity {
         View btDelete;
         View btLeft;
         SwipeMenuLayout sml;
-        public MyViewHolder(View itemView) {
+        public SimpleRvViewHolder(View itemView) {
             super(itemView);
             tvName = (TextView) itemView.findViewById(R.id.tvName);
             tvSwipeEnable = (TextView) itemView.findViewById(R.id.tvSwipeEnable);
